0810a6dfc1220b4ac4b9007232a0f6e319a6246d
Mugen1992 <75086660+Mugen1992@users.noreply.github.com>
2025-08-11 14:53:45 +0500

    Create InteractableLever.cs

---
+++ Code/InteractableLever.cs
@@ -0,0 +1,7 @@
+// Реализация IInteractable: при активации меняем состояние связанного механизма (платформа/дверь).
+
+public class InteractableLever : MonoBehaviour, IInteractable {
+    public void Interact() {
+        // TODO: Change linked mechanism state
+    }
+}
