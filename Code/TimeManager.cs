ab41b4cc272ec9ab7d6ba1d0a00c7d14cab1d7bb
Mugen1992 <75086660+Mugen1992@users.noreply.github.com>
2025-08-11 14:57:46 +0500

    Update TimeManager.cs

--- Code/TimeManager.cs
+++ Code/TimeManager.cs
@@ -1,9 +1,30 @@
-// Локальная манипуляция временем.
-// При активации способности: создаём сферу-область, находим ITimeAffectable, меняем их локальный timescale.
-// Для отката: у объектов с "PositionHistory" проигрываем буфер позиций назад N миллисекунд.
+using UnityEngine;
+using System.Collections.Generic;
+
+public enum TimeMode { Slow, Speed, Rewind }
 
 public class TimeManager : MonoBehaviour {
-    // TODO: SpawnTimeSphere(position, radius, mode: Slow/Speed/Rewind, duration)
-    // TODO: Register/Unregister ITimeAffectable
-    // TODO: RewindObject(obj, msBack)
+    public float slowFactor = 0.5f;
+    public float speedFactor = 1.5f;
+    public float duration = 3f;
+
+    public void SpawnTimeSphere(Vector3 position, float radius, TimeMode mode) {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, radius);
+        foreach (var col in colliders) {
+            ITimeAffectable obj = col.GetComponent<ITimeAffectable>();
+            if (obj != null) {
+                switch (mode) {
+                    case TimeMode.Slow:
+                        obj.SetTimeScale(slowFactor, duration);
+                        break;
+                    case TimeMode.Speed:
+                        obj.SetTimeScale(speedFactor, duration);
+                        break;
+                    case TimeMode.Rewind:
+                        obj.Rewind(1.5f); // 1.5 seconds back
+                        break;
+                }
+            }
+        }
+    }
 }
